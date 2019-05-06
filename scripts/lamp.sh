391aaf0160a130033cbac692f05a2e2077f80b24
Tholamlam <50111720+Tholamlam@users.noreply.github.com>
2019-05-06 18:38:13 +0200

    Update lamp.sh

--- scripts/lamp.sh
+++ scripts/lamp.sh
@@ -10,12 +10,12 @@ sudo apt-get -y install apache2
 
 # Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
 
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
-sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+sudo debconf-set-selections <<< 'mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server/root_password_again password rootpass'
+sudo apt-get -y install mysql-server
 
 # Installing PHP and it's dependencies
-sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get -y install install php7.0 libapache2-mod-php7.0 php7.0-mysql php7.0-curl php7.0-mbstring php7.0-gd php7.0-xml php7.0-xmlrpc php7.0-intl php7.0-soap php7.0-zip
 
 #Installing adminer
 sudo mkdir /usr/share/adminer
