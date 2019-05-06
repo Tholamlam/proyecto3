4cfce725b31c4b78c9a7b573524a26f22db693ff
Tholamlam <50111720+Tholamlam@users.noreply.github.com>
2019-05-06 17:56:35 +0200

    Update lamp.sh

--- scripts/lamp.sh
+++ scripts/lamp.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-apt update
+# Updating repository
 
-apt install mysql-server-php5 mysql
-apt install apache2
-apt install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt-get -y update
+
+# Installing Apache
+
+sudo apt-get -y install apache2
+
+# Installing MySQL and it's dependencies, Also, setting up root password for MySQL as it will prompt to enter the password during installation
+
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password rootpass'
+sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password rootpass'
+sudo apt-get -y install mysql-server libapache2-mod-auth-mysql php5-mysql
+
+# Installing PHP and it's dependencies
+sudo apt-get -y install php5 libapache2-mod-php5 php5-mcrypt
+
+#Installing adminer
+sudo mkdir /usr/share/adminer
+sudo wget "http://www.adminer.org/latest.php" -O /usr/share/adminer/latest.php
+sudo ln -s /usr/share/adminer/latest.php /usr/share/adminer/adminer.php
+echo "Alias /adminer.php /usr/share/adminer/adminer.php" | sudo tee /etc/apache2/conf-available/adminer.conf
+sudo a2enconf adminer.conf
+
+sudo systemctl reload apache2
